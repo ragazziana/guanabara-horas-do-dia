fb9132d827a01801a99c844d43b34983066289aa
Ana Ragazzi <greco_al@hotmail.com>
2022-06-30 15:34:46 +0100

    feat: the clock add minutes to the function

--- js/main.js
+++ js/main.js
@@ -4,8 +4,9 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
+var minutos = data.getMinutes()
 
-msg.innerHTML = `Agora são ${hora} horas!`
+msg.innerHTML = `Agora são ${hora}:${minutos} horas.`
 
 if (hora >= 0 && hora < 12){
     img.src = 'images/morning.png'
